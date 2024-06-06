cae4ede262c0d918341d0bdf57cbdf408b4e4428
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-06-06 15:11:53 +0200

    Add processing report send to ban-plateforme

--- src/bal-converter/helpers/check-if-bal-use-ban-id.ts
+++ src/bal-converter/helpers/check-if-bal-use-ban-id.ts
@@ -15,17 +15,14 @@ const checkIfBALUseBanId = (bal: Bal, version?: BalVersion) => {
     // If at least one of the IDs is present, it means that the BAL address is using BanID
     if (districtID || mainTopoID || addressID) {
       if (!districtID){
-        logger.info(`Missing districtID for bal address ${JSON.stringify(balAdresse)}`);
         throw new Error(`Missing districtID for bal address ${JSON.stringify(balAdresse)}`);
       }
 
       if (!mainTopoID){
-        logger.info(`Missing mainTopoID for bal address ${JSON.stringify(balAdresse)}`);
         throw new Error(`Missing mainTopoID for bal address ${JSON.stringify(balAdresse)}`);
       }
 
       if (balAdresse.numero !== Number(IS_TOPO_NB) && !addressID){
-        logger.info(`Missing addressID for bal address ${JSON.stringify(balAdresse)}`);
         throw new Error(`Missing addressID for bal address ${JSON.stringify(balAdresse)}`);
       }
       balAdresseUseBanId++
