a6d93b14651d83b0e1bb56acfc068f4d17f2a778
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2025-01-02 15:32:49 +0100

    Added multiple district ids handler

--- src/compute-from-cog.ts
+++ src/compute-from-cog.ts
@@ -1,10 +1,7 @@
 // Get revision from dump-api (api de dépôt)
-import { logger } from './utils/logger.js';
-import {
-  getRevisionFromDistrictCOG,
-  getRevisionFileText,
-} from './dump-api/index.js';
-import { sendBalToBan } from './bal-converter/index.js';
+import { logger } from "./utils/logger.js";
+import { getRevisionData } from "./dump-api/index.js";
+import { sendBalToBan } from "./bal-converter/index.js";
 import {
   getDistrictFromCOG,
   partialUpdateDistricts,
@@ -16,7 +13,9 @@ import {
   getBalVersion,
 } from './bal-converter/helpers/index.js';
 
-import acceptedCogList from './accepted-cog-list.json' with { type: 'json' };
+import acceptedCogList from "./accepted-cog-list.json" with { type: "json" };
+import { BalAdresse } from "./types/bal-types.js";
+import { BanDistrict } from "./types/ban-types.js";
 
 export const computeFromCog = async (
   cog: string,
@@ -35,67 +34,86 @@ export const computeFromCog = async (
 
   logger.info(`District cog ${cog} is part of the whitelist.`);
 
-  // Get BAL (csv) from dump-api
-  const revision = await getRevisionFromDistrictCOG(cog);
-  const revisionFileText = await getRevisionFileText(revision.id);
+  // Get BAL text data from dump-api
+  const { revision, balTextData: balCsvData } = await getRevisionData(cog);
 
   // Convert csv to json
-  const bal = csvBalToJsonBal(revisionFileText);
+  const bal = csvBalToJsonBal(balCsvData);
 
   // Detect BAL version
   const version = getBalVersion(bal);
   logger.info(`District cog ${cog} is using BAL version ${version}`);
 
-  // Get related districts from our main BAN DB
-  const districtResponseRaw = await getDistrictFromCOG(cog);
-  if (!districtResponseRaw.length) {
+  const districts: BanDistrict[] = await getDistrictFromCOG(cog);
+  if (!districts.length) {
     throw new Error(`No district found with cog ${cog}`);
-  } else if (districtResponseRaw.length > 1) {
-    throw new Error(
-      `Multiple district found with cog ${cog}. Behavior not handled yet.`
-    );
   }
 
-  const { id } = districtResponseRaw[0];
+  const districtIDsFromDB = districts.map((district) => district.id);
 
   // Check if bal is using BanID
   // If not, sending process to ban-plateforme legacy API
   // If the use of IDs is partial, throwing an error
-  const useBanId = await validator(bal, version, { cog, districtID: id });
+  const useBanId = await validator(districtIDsFromDB, bal, version, { cog });
 
   if (!useBanId) {
     logger.info(
-      `District id ${id} (cog: ${cog}) does not use BanID: sending BAL to legacy compose...`
+      `District cog: ${cog} does not use BanID: sending BAL to legacy compose...`
     );
     return await sendBalToLegacyCompose(cog, forceLegacyCompose as string);
   } else {
-    logger.info(`District id ${id} (cog: ${cog}) is using banID`);
-    // Patch district meta with revision data from dump-api (id and date)
-    const districtUpdate = {
-      id,
-      meta: {
-        bal: {
-          idRevision: revision.id,
-          dateRevision: revision.publishedAt,
-        },
+    // Split BAL by district ID to handle multiple districts in a BAL
+    const splitBalPerDistictID = bal.reduce(
+      (acc: { [key: string]: BalAdresse[] }, balAdresse) => {
+        if (balAdresse.id_ban_commune) {
+          if (!acc[balAdresse.id_ban_commune]) {
+            acc[balAdresse.id_ban_commune] = [];
+          }
+          acc[balAdresse.id_ban_commune].push(balAdresse);
+        }
+        return acc;
       },
-    };
-    await partialUpdateDistricts([districtUpdate]);
-
-    const result = (await sendBalToBan(bal)) || {};
-
-    if (!Object.keys(result).length) {
-      const response = `District id ${id} (cog: ${cog}) not updated in BAN BDD. No changes detected.`;
-      logger.info(response);
-      return response;
-    }
-
-    logger.info(
-      `District id ${id} (cog: ${cog}) updated in BAN BDD. Response body : ${JSON.stringify(
-        result
-      )}`
+      {}
     );
 
-    return result;
+    logger.info(`District cog: ${cog} is using banID`);
+
+    const results = [];
+    for (let i = 0; i < Object.keys(splitBalPerDistictID).length; i++) {
+      const [id, bal] = Object.entries(splitBalPerDistictID)[i];
+      try {
+        // Update District meta with revision data from dump-api (id and date)
+        const districtUpdate = {
+          id,
+          meta: {
+            bal: {
+              idRevision: revision.id,
+              dateRevision: revision.publishedAt,
+            },
+          },
+        };
+        await partialUpdateDistricts([districtUpdate]);
+
+        const result = (await sendBalToBan(bal)) || {};
+
+        if (!Object.keys(result).length) {
+          const response = `District id ${id} (cog: ${cog}) not updated in BAN BDD. No changes detected.`;
+          logger.info(response);
+          results.push(response);
+        } else {
+          logger.info(
+            `District id ${id} (cog: ${cog}) updated in BAN BDD. Response body : ${JSON.stringify(
+              result
+            )}`
+          );
+          results.push(result);
+        }
+      } catch (error) {
+        const { message } = error as Error;
+        logger.error(message);
+        results.push(`Error for district ${id} (cog: ${cog}) : ${message}`);
+      }
+    }
+    return results;
   }
 };
